6820560f831571002b447e93aafc75e943f05d4d
Jenny Kim <nkim54@dtc01l0840-21.ad.bcit.ca>
2025-06-17 13:53:40 -0700

    Set up GitHub Actions FTP deployment

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,10 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: './', 
+  base: './',
   plugins: [react()],
   build: {
     outDir: 'dist',
+    assetsDir: '', // ✅ 중요: assets를 dist 바로 아래에 빌드되게 함
   },
 });
