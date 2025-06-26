e66659535ecc1bf690e694196f12adc0fb11db67
jenny950124 <jenny237k@gmail.com>
2025-06-25 21:56:06 -0700

    refresh error fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import ScrollToTop from './components/ScrollToTop'; 
 import Layout from "./components/Layout"; 
 import Home from "./components/Home";     
@@ -10,8 +10,8 @@ import ProjectDetail from "./components/ProjectDetail";
 
 function App() {
   return (
-    <BrowserRouter>
-      <ScrollToTop /> {/* ✅ 정확히 Routes 위에 있어야 함 */}
+   <HashRouter>
+    <ScrollToTop />
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
@@ -21,7 +21,7 @@ function App() {
           <Route path="contact" element={<Contact />} />
         </Route>
       </Routes>
-    </BrowserRouter>
+  </HashRouter>
   );
 }
 
