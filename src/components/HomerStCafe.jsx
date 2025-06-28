54cecf339a6b31271a0133355fb5d4262be0d0d3
jenny950124 <jenny237k@gmail.com>
2025-06-27 18:49:07 -0700

    Fix: corrected import casing for HomerStCafe.css

--- src/components/HomerStCafe.jsx
+++ src/components/HomerStCafe.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import './Homerstcafe.css';
+import './HomerStCafe.css';
 import useScrollFadeIn from '../hooks/useScrollFadeIn';
 
 import banner from '../assets/app/Banner.png';
